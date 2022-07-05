d8c8f112bd84da024dabdbf0255218816552e7df
Lucio Schiavoni <luciosc1798@gmail.com>
2022-07-05 19:59:26 -0300

    Entrega-2

---
+++ app.js
@@ -0,0 +1,30 @@
+
+let capital
+let porcentaje
+let apalancamiento
+
+do {
+    capital = parseInt(prompt("Ingrese su capital a invertir"))
+    porcentaje = parseInt(prompt("Ingrese su porcentaje de ganancia"))
+    apalancamiento = parseInt(prompt("Ingrese su apalancamiento"))
+
+if( apalancamiento > 20){
+    alert("Su apalancamiento es demasiado riesgoso, use un apalancamiento menor a 20")
+    
+}
+
+
+
+
+} while (isNaN(capital) || isNaN(porcentaje) || isNaN(apalancamiento));
+
+
+
+
+function operacion(){
+operacion =  ((porcentaje*apalancamiento)*capital)/100
+}
+
+
+operacion();
+console.log("su ganancia de inversion fue de " +operacion +" Usd con un apalancamiento de "+apalancamiento +"x" )
